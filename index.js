5aa88e39b9736e35babc1bfeca5032c923e972e1
Noah Schefer <nbschefer@gmail.com>
2019-11-06 14:42:14 -0500

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const express = require('express');
+const session = require('express-session');
+const app = express();
+
+app.use(session({
+  secret: 'my-secrety-secret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use((req, res, next) => {
+  console.log('SESSION: ', req.session);
+  next();
+})
+
+app.use((req, res, next) => {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('counter', ++req.session.counter);
+  next();
+})
+
+app.get('/', (req, res, next) => {
+  res.send('Hello');
+})
+
+app.listen(8000, () => console.log('Listening on port 8000'))
